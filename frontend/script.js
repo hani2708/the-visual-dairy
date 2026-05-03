90305276e86f0825ac309caab6ba4537fa85c8e3
hani2708 <hasinisa2007@gmail.com>
2026-05-03 08:07:17 +0530

    Update script.js  with username

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@ function login() {
     let user = document.getElementById("username").value;
     let pass = document.getElementById("password").value;
 
-    if (user === "hasini" && pass === "1234") {
+    if (user === "gptcmy" && pass === "1234") {
         localStorage.setItem("user", user);
         window.location.href = "gallery.html";
     } else {
